536764617499b4daecf796de762eb3b9b6886098
Gilbert Acosta <acostagilberto95@gmail.com>
2020-10-05 06:56:27 -0400

    Clase Tipos de datos

---
+++ 01-Basico/02_datatypes.dart
@@ -0,0 +1,70 @@
+main() {
+  // ============================ Numeros
+
+  int a = 10;
+  double b = 5.5;
+  int c;
+
+  int x = 10, y = 20, z = 30;
+
+  int _a = 30;
+
+  double $b = 45.55;
+
+  // print($b); 
+  // print(y); 
+  // print(z);  
+
+  // ============================ Strings - Cadenas de caracteres
+
+  String nombre = 'Tony';
+  String nombre2 = "Tony";
+  String nombre3 = 'O\'Connor'; // O'Connor
+
+  String multilinea = '''
+  Hola Mundo
+  Como Estan?
+  ''';
+
+  // print(multilinea);
+
+  // ============================ Booleans
+
+  bool activo = true;
+  bool corriendo;
+
+  activo = !activo;
+
+  // print(activo);
+
+  // ============================ Listas - Arreglos
+
+  // List<String> personajes = ['Superman', 'Batman'];
+  List<String> personajes = new List();
+
+  personajes..add('Batman')
+            ..add('Superman')
+            ..add('Batman');
+
+  // print(personajes);
+
+  List<String> villanos = new List(3);
+
+  // villanos.addAll(['Lex', 'Red Skull', 'Doom']);
+  villanos[0] = 'Lex';
+  villanos[1] = 'Red Skull';
+  villanos[2] = 'Doom';
+
+  // print(villanos);
+
+
+  // ============================ Sets
+
+  Set<String> villanos2 = {'Lex', 'Red Skull', 'Doom'};
+
+  villanos2.add('Lex');
+
+  print(villanos2.first);
+
+  // Mapas
+}
